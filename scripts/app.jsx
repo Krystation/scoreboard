fdfbea13af65e5f4a2f05eed466d538dea939ab2
K-Flo <krystianflores@gmail.com>
2018-05-03 00:03:59 -0700

    Added styles to scoreboard

--- scripts/app.jsx
+++ scripts/app.jsx
@@ -2,17 +2,28 @@ function Application() {
   return (
     <div className="application">
       <div className="header">
-        <h1>Scoreboard</h1>
+        <h1>SCOREBOARD</h1>
       </div>
       <div className="players">
         <div className="player">
           <span className="player_name">Krys</span>
+          <div className="player_score">
+            <div className="counter">
+              <button className="counter_action decrement"> - </button>
+              <div className="counter_score"> 31 </div>
+              <button className="counter_action increment"> + </button>
+            </div>
+          </div>
         </div>
-        <div className="player_score">
-          <div className="counter">
-            <button className="counter_action decrement"> - </button>
-            <div className="counter_score"> 31 </div>
-            <button className="counter_action increment"> + </button>
+        {/*Second Player*/}
+        <div className="player">
+          <span className="player_name">John Wick</span>
+          <div className="player_score">
+            <div className="counter">
+              <button className="counter_action decrement"> - </button>
+              <div className="counter_score"> 13 </div>
+              <button className="counter_action increment"> + </button>
+            </div>
           </div>
         </div>
       </div>
